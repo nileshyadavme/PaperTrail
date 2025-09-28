9bdfd94559f91f82dc5550b75667698f9dafdf1e
nilesh <contact.nileshy@gmail.com>
2025-09-28 04:25:33 +0000

    refactor: remove unused backend files and dependencies

--- backend/app/services/resume-parser.py
+++
@@ -1,6 +0,0 @@
-import os
-os.environ["SPACY_MODEL"] = "en_core_web_sm"
-
-from pyresparser import ResumeParser
-data = ResumeParser("./backend/app/assets/resume.pdf").get_extracted_data()
-print(data)
